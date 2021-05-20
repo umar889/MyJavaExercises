a31ecf1357d785c91da26eff09d5c854070d3427
umar abdul <umaruru04@gmail.com>
2021-05-20 17:57:57 +0100

    DisplayingShapesWithAsterisks

---
+++ test/africa/semicolon/DisplayingShapesWithAsterisks.java
@@ -0,0 +1,18 @@
+package africa.semicolon;
+
+public class DisplayingShapesWithAsterisks {
+    public static void main(String...args){
+
+        System.out.println("*********     ***       *       *    ");
+        System.out.println("*       *   *     *    ***     * *   ");
+        System.out.println("*       *  *       *  *****   *   *  ");
+        System.out.println("*       *  *       *    *    *     * ");
+        System.out.println("*       *  *       *    *   *       *");
+        System.out.println("*       *  *       *    *    *     * ");
+        System.out.println("*       *  *       *    *     *   *  ");
+        System.out.println("*       *   *     *     *      * *   ");
+        System.out.println("*********     ***       *       *    ");
+    }
+}
+
+
