4366c72d5cefb4cdeb5400c96214cfaff42b716b
umar abdul <umaruru04@gmail.com>
2021-06-29 10:13:56 +0100

    added files;

---
+++ src/africa/Chaptereight/Main.java
@@ -0,0 +1,11 @@
+package africa.Chaptereight;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        String Question1 = "Enter A if A is .....or Enter B if B is .....";
+        String Question2 =  "Enter A if A is .....or Enter B if B is .....";
+
+    }
+}
